157ea050107364c1f4577db0df9c64e9c6bcda6d
Garrett Davies <grdavies99@gmail.com>
2022-02-22 09:34:37 -0700

    first real commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import BGImageContainer from './BGImageContainer';
+
+const displayEmojiName = event => alert(event.target.id);
+const emojis = [
+  {
+    emoji: '😀',
+    name: "test grinning face"
+  },
+  {
+    emoji: '🎉',
+    name: "party popper"
+  },
+  {
+    emoji: '💃',
+    name: "woman dancing"
+  }
+];
+
+function Header(props){
+  return(
+        <nav>
+            <h1 id={props.greeting}>{props.title}</h1>
+            <ul>
+                <li>Products</li>
+                <li>Support</li>
+                <li>About</li>
+            </ul>
+        </nav>
+  );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const greeting = "greeting";
+  const displayAction = false;
+  return(
+    <div className="container">
+      <Header greeting={greeting} title="Reqursive"/>
+      {displayAction && <p></p>}
+      <BGImageContainer />
+      <ul>
+        {
+          emojis.map(emoji => (
+            <li key={emoji.name}>
+              <button
+                onClick={displayEmojiName}
+              >
+                <span role="img" aria-label={emoji.name} id={emoji.name}>{emoji.emoji}</span>
+              </button>
+            </li>
+          ))
+        }
+      </ul>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/BGImageContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './BGImageContainer.css';
+
+export default class BGImageContainer extends Component {
+    render(){
+        return(
+            <div class="BgImage">
+                <h3>It cost's businesses $30,000 to replace an employee that makes the national median salary,<br/> its time to fix that with Reqursive</h3>
+            </div>
+        );
+    }
+}
